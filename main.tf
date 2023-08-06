288cfbcb0faa9e62779a9e9557d25d77dca4a3fa
Slovink DevOps <devops@slovink.com>
2023-08-06 22:53:04 +0530

    change configuration for gke cluster

--- main.tf
+++ main.tf
@@ -26,21 +26,21 @@ resource "google_container_node_pool" "node_pool" {
   project            = var.project_id
   location           = var.location
   cluster            = join("", google_container_cluster.primary.*.id)
-  node_count = var.initial_node_count
+  node_count         =  var.initial_node_count
 
-  autoscaling {
-    min_node_count  = var.min_node_count
-    max_node_count  = var.max_node_count
-    location_policy = var.location_policy
-  }
+  # autoscaling {
+  #   min_node_count  = var.min_node_count
+  #   max_node_count  = var.max_node_count
+  #   location_policy = var.location_policy
+  # }
 
-  management {
-    auto_repair  = var.auto_repair
-    auto_upgrade = var.auto_upgrade
-  }
+  # management {
+  #   auto_repair  = var.auto_repair
+  #   auto_upgrade = var.auto_upgrade
+  # }
 
   node_config {
-    image_type      = var.image_type
+    # image_type      = var.image_type
     machine_type    = var.machine_type
     service_account = var.service_account
     disk_size_gb   = var.disk_size_gb
