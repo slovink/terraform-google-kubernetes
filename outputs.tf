c0b73d11994efa6677a2d407f3ee45af0772ba9e
Slovink DevOps <devops@slovink.com>
2024-01-13 14:53:14 +0100

    adding output for kubeconfig

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "endpoint" {
 output "cluster_ca_certificate" {
   value = join("", google_container_cluster.primary.*.id)
 }
+
+output "kubeconfig" {
+  value = google_container_cluster.primary.kube_master_auth[0].client_certificate_config[0].client_certificate
+}
