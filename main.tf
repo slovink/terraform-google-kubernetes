7dd3a7d5474ef1a8ba1ea5ac9231cf378f52b536
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-02 03:20:10 -0500

    add image type in node config

--- main.tf
+++ main.tf
@@ -57,11 +57,7 @@ resource "google_container_node_pool" "node_pool" {
     tags            = ["gke-node"]
     labels = {
       environment = "prod"
-    }
-    workload_metadata_config {
-        mode = "GKE_METADATA"  # Explicitly set metadata mode (alternatively "GCE_METADATA")
-      }
-    
+    }    
 
   }
 
