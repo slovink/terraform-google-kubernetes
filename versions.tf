f3d698fab77b09a3be00f3493da795d0bdfc209d
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-02 03:45:44 -0500

    upgrade goggle provider version

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">= 1.7.2"
-
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0.0, < 6.0.0"
+      source = "hashicorp/google"
+      version = "6.8.0"
     }
+  }
     null = {
       source  = "hashicorp/null"
       version = "3.2.2"
