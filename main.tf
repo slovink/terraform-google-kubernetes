dd7781af755cafbea0c558dafdb5ec159aba2972
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-02 02:01:10 -0500

    add image type in node config

--- main.tf
+++ main.tf
@@ -54,6 +54,7 @@ resource "google_container_node_pool" "node_pool" {
     disk_size_gb   = var.disk_size_gb
     disk_type       = var.disk_type
     preemptible     = var.preemptible
+    image_type      = var.image_type
 
   }
 
