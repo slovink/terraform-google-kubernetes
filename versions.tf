ceeb6da6d830456f69e24a6b704b82559693097b
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-02 03:18:13 -0500

    add image type in node config

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.20.0"
+      version = ">= 3.50, < 6.0.0"
     }
     null = {
       source  = "hashicorp/null"
