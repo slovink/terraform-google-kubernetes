8c06aab268789a89e96345ce68d1f805c1824c81
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-15 10:16:29 -0500

    remove the default node pool

--- network.tf
+++ network.tf
@@ -5,5 +5,5 @@ data "google_compute_subnetwork" "gke_subnetwork" {
   count   = var.add_cluster_firewall_rules ? 1 : 0
   name               = var.subnetwork
   region  = local.region
-  project = local.project_id
+  project = var.project_id
 }
