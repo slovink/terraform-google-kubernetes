f621b135ac1a8765d757f2c1aea542c5546108db
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-15 06:22:09 -0500

    add more feature to gke cluster

--- locals.tf
+++ locals.tf
@@ -3,6 +3,7 @@ locals {
 
     location = var.regional ? var.region : var.zones[0]
     region   = var.regional ? var.region : join("-", slice(split("-", var.zones[0]), 0, 2))
+    zone_count                  = length(var.zones)
     cluster_network_policy = var.network_policy ? [{
     enabled  = true
     provider = var.network_policy_provider
