29fa09c84e031094be9ca7042fb9857d00716f6d
Vick Stork <admins@Workstation-ltd.o05rithub0vuvgqrbjzs2svl2h.bx.internal.cloudapp.net>
2025-02-16 05:43:50 -0500

    add more feature to gke cluster

--- locals.tf
+++ locals.tf
@@ -1,18 +1,6 @@
 /******************************************
-  Get available zones in region
+  Locals
  *****************************************/
-data "google_compute_zones" "available" {
-  count = local.zone_count == 0 ? 1 : 0
-  project = var.project_id
-  region  = var.region
-}
-
-resource "random_shuffle" "available_zones" {
-  count = local.zone_count == 0 ? 1 : 0
-
-  input        = data.google_compute_zones.available[0].names
-  result_count = 3
-}
 
 locals {
 
