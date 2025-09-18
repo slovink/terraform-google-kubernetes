e231eae699c673557673b96e812469c3fafc31ac
krishna <krishan.yadav@slovink.com>
2025-09-18 12:10:52 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.2"
+      version = ">= 4.0, < 6.0"
     }
   }
 }
