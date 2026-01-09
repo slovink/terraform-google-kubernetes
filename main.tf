d2305115d71c0d08e6b77ef0696dc241dcfd41ea
vinodyadav9 <vinod.yadav@slovink.com>
2026-01-09 22:13:25 +0530

    fix AUTOSCALING DISABLED

--- main.tf
+++ main.tf
@@ -64,13 +64,10 @@ resource "google_container_node_pool" "node_pool" {
   location = var.location
   cluster  = join("", google_container_cluster.primary[*].id)
 
-  node_locations = lookup(each.value, "node_locations", "") != ""
-  ? split(",", each.value["node_locations"])
-  : null
+  node_locations = lookup(each.value, "node_locations", "") != "" ? split(",", lookup(each.value, "node_locations", "")) : null
 
-  version = lookup(each.value, "auto_upgrade", local.default_auto_upgrade)
-  ? google_container_cluster.primary[0].min_master_version
-  : lookup(each.value, "version", google_container_cluster.primary[0].min_master_version)
+
+  version = lookup(each.value, "auto_upgrade", local.default_auto_upgrade) ? google_container_cluster.primary[0].min_master_version : lookup(each.value, "version", google_container_cluster.primary[0].min_master_version)
 
   # -------------------------------
   # CREATE TIME ONLY
